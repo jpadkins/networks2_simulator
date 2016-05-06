d1e11ab88798f600e3487f8aceb1a6020a161f1a
jacobpadkins <jpadkins@smu.edu>
2016-05-06 14:58:16 -0500

    changed colors

--- src/signal.rs
+++ src/signal.rs
@@ -7,7 +7,4 @@ impl Signal {
     pub fn friis(pt: &f64, gt: &f64, gr: &f64, lamda: &f64, d: &f64) -> f64 {
         pt*gt*gr * ((lamda / (4.0*f64::consts::PI*d)).powi(2))
     }
-    pub fn aplitude(frequency: f64, distance: f64) -> f64 {
-        distance / frequency
-    }
 }
